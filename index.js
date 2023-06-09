b77d7f55478c95aa61cff5a2465f624e85f7c921
Imran-A-K <imran.k.professional@gmail.com>
2023-06-09 18:15:23 +0600

    added jwt validater function

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+const validateJWT = async(req,res,next) =>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error: true, message: 'You are not authorized'})
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if(error){
+        return res.status(401).send({error: true, message: 'You are not authorized'})
+      }
+      
+      req.decoded = decoded;
+      next() 
+    })
+  }
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
