7df234d43f20355ff203793de9e7e1a840609448
Imran-A-K <imran.k.professional@gmail.com>
2023-06-09 17:55:38 +0600

    initialized the server

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 4000
 
+
+app.get('/', (req,res)=>{
+    res.send('Camp ninja is operating')
+})
+app.listen(port,() => {
+    console.log(`Camp ninja is operating on port ${port}`)
+})
+
