2e26ce4b266fbc51081f08371601f59dbec540cd
Imran-A-K <imran.k.professional@gmail.com>
2023-06-09 17:50:49 +0600

    set up middlewares

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+
+
+const app = express()
+const port = process.env.PORT || 4000
+
